53bf0a66b9680550a63d41662c8f34b5d9577104
Akashkr28 <email.akash2010@gmail.com>
2025-12-15 22:15:22 +0530

    PDF Upload error fix

--- client/src/components/PDFEditor.jsx
+++ client/src/components/PDFEditor.jsx
@@ -6,10 +6,12 @@ import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 
 // Worker Setup
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
-).toString();
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+//   'pdfjs-dist/build/pdf.worker.min.mjs',
+//   import.meta.url,
+// ).toString();
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const PDFEditor = ({ onPositionChange, pdfFile, addedSignatures, signatureUrl }) => {
     const containerRef = useRef(null);
